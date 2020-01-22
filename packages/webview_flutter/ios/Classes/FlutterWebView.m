235b79f0699e3e403d420c8947b1cfc1df9101b0
Henri Beck <henri.beck@aboutyou.de>
2020-01-22 15:04:02 +0100

    Remove broken iOS code

--- packages/webview_flutter/ios/Classes/FlutterWebView.m
+++ packages/webview_flutter/ios/Classes/FlutterWebView.m
@@ -106,9 +106,6 @@
 
     if (@available(iOS 11.0, *)) {
       _webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
-      if (@available(iOS 13.0, *)) {
-        _webView.scrollView.automaticallyAdjustsScrollIndicatorInsets = NO;
-      }
     }
 
     [self applySettings:settings];
